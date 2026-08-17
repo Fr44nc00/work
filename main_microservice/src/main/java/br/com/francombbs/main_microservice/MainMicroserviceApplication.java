d888c936638e82b83fa3db4de5ad2bcfd46bcf9d
Franco <mateus.franco@dof.com>
2026-08-17 14:50:02 -0300

    Adicionando começo do projeto do prof

---
+++ main_microservice/src/main/java/br/com/francombbs/main_microservice/MainMicroserviceApplication.java
@@ -0,0 +1,21 @@
+package br.com.francombbs.main_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+// VVV Transforma o projeto num servidor próprio
+@EnableEurekaServer
+// VVV Ativa o config server
+@EnableConfigServer
+public class MainMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainMicroserviceApplication.class, args);
+	}
+
+}
+
+// Aula do dia 24, 36:30
---
+++ main_microservice/src/test/java/br/com/francombbs/main_microservice/MainMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.francombbs.main_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
